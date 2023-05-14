cee62e08a46cad72a47a84ff260ab3817f4a2944
BrewertonSantos <brewerton.santos@outlook.com>
2023-05-14 18:10:24 -0300

    feat: added styles to new components.

---
+++ PadWiki.WebClient/wwwroot/js/app.js
@@ -0,0 +1,23 @@
+﻿window.addScrollListener = (element, dotnetHelper) => {
+    element.onscroll = () => {
+        var isAtStart = element.scrollLeft === 0;
+        var isAtEnd = element.scrollWidth - element.scrollLeft === element.clientWidth;
+        dotnetHelper.invokeMethodAsync('HandleScroll', isAtStart, isAtEnd);
+    };
+};
+
+window.scrollElement = (element, amount) => {
+    const newScrollPosition = element.scrollLeft + amount;
+    element.scrollTo({
+        left: newScrollPosition,
+        behavior: 'smooth'
+    });
+}
+
+window.isScrollAtStart = (element) => {
+    return element.scrollLeft === 0;
+};
+
+window.isScrollAtEnd = (element) => {
+    return element.scrollLeft + element.clientWidth === element.scrollWidth;
+};
